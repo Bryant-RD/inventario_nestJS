86b7ae950d28826429fa99dc0e738078881eae94
Bryant-RD <briant04.rt@gmail.com>
2025-10-18 15:48:26 -0400

    4to intento

--- cucumber.js
+++ cucumber.js
@@ -1,7 +1,25 @@
+// module.exports = {
+//   default: {
+//     requireModule: ['tsconfig-paths/register', 'ts-node/register'],
+//     require: ['features/steps_definitions/**/*.ts', 'features/support/**/*.ts'],
+//     paths: ['features/**/*.feature'],
+//     format: ['progress-bar', 'json:reports/cucumber-report.json'],
+//     worldParameters: {},
+//   },
+// };
+
+
 module.exports = {
   default: {
-    requireModule: ['tsconfig-paths/register', 'ts-node/register'],
-    require: ['features/steps_definitions/**/*.ts', 'features/support/**/*.ts'],
+    require: [
+      './test/jest.setup.ts',                // <-- primero el setup para crypto
+      'features/support/**/*.ts',            // <-- hooks y world
+      'features/steps_definitions/**/*.ts'   // <-- luego los steps
+    ],
+    requireModule: [
+      'ts-node/register',                    // <-- correr TS
+      'tsconfig-paths/register'              // <-- soportar "paths" en tsconfig
+    ],
     paths: ['features/**/*.feature'],
     format: ['progress-bar', 'json:reports/cucumber-report.json'],
     worldParameters: {},
